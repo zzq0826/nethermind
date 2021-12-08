ab8c62d55790a26238ea2604da18312bb79a3445
lukasz.rozmej <lukasz.rozmej@gmail.com>
2021-12-08 17:49:03 +0100

    Don't save NetworkDiagTracer to file

--- src/Nethermind/Nethermind.Core/NetworkDiagTracer.cs
+++ src/Nethermind/Nethermind.Core/NetworkDiagTracer.cs
@@ -61,7 +61,7 @@ namespace Nethermind.Core
             }
 
             string contents = stringBuilder.ToString();
-            File.WriteAllText(NetworkDiagTracerPath, contents);
+            // File.WriteAllText(NetworkDiagTracerPath, contents);
             _logger.Info(contents);
         }
 
