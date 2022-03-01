48dd52f1350ba77e78dde7b8ec5298f3bcb977f3
lukasz.rozmej <lukasz.rozmej@gmail.com>
2022-03-01 17:05:03 +0100

    Disable existing check

--- src/Nethermind/Nethermind.Synchronization/FastSync/StateSyncFeed.cs
+++ src/Nethermind/Nethermind.Synchronization/FastSync/StateSyncFeed.cs
@@ -471,24 +471,24 @@ namespace Nethermind.Synchronization.FastSync
                     return AddNodeResult.AlreadySaved;
                 }
 
-                object lockToTake = syncItem.NodeDataType == NodeDataType.Code ? _codeDbLock : _stateDbLock;
-                lock (lockToTake)
-                {
-                    IDb dbToCheck = syncItem.NodeDataType == NodeDataType.Code ? _codeDb : _stateDb;
-                    Interlocked.Increment(ref _data.DbChecks);
-                    bool keyExists = dbToCheck.KeyExists(syncItem.Hash);
-
-                    if (keyExists)
-                    {
-                        if (_logger.IsTrace) _logger.Trace($"Node already in the DB - skipping {syncItem.Hash}");
-                        _alreadySaved.Set(syncItem.Hash);
-                        Interlocked.Increment(ref _data.StateWasThere);
-                        _branchProgress.ReportSynced(syncItem, NodeProgressState.AlreadySaved);
-                        return AddNodeResult.AlreadySaved;
-                    }
-
-                    Interlocked.Increment(ref _data.StateWasNotThere);
-                }
+                // object lockToTake = syncItem.NodeDataType == NodeDataType.Code ? _codeDbLock : _stateDbLock;
+                // lock (lockToTake)
+                // {
+                //     IDb dbToCheck = syncItem.NodeDataType == NodeDataType.Code ? _codeDb : _stateDb;
+                //     Interlocked.Increment(ref _data.DbChecks);
+                //     bool keyExists = dbToCheck.KeyExists(syncItem.Hash);
+                //
+                //     if (keyExists)
+                //     {
+                //         if (_logger.IsTrace) _logger.Trace($"Node already in the DB - skipping {syncItem.Hash}");
+                //         _alreadySaved.Set(syncItem.Hash);
+                //         Interlocked.Increment(ref _data.StateWasThere);
+                //         _branchProgress.ReportSynced(syncItem, NodeProgressState.AlreadySaved);
+                //         return AddNodeResult.AlreadySaved;
+                //     }
+                //
+                //     Interlocked.Increment(ref _data.StateWasNotThere);
+                // }
 
                 bool isAlreadyRequested;
                 lock (_dependencies)
