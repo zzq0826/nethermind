ecd58ebfe41d9efd38f543064fbc479653db864f
MarekM25 <marekm2504@gmail.com>
2022-10-06 12:24:03 +0200

    unblock rpc?

--- src/Nethermind/Nethermind.Init/Steps/StartBlockProducer.cs
+++ src/Nethermind/Nethermind.Init/Steps/StartBlockProducer.cs
@@ -25,7 +25,7 @@ using Nethermind.Logging;
 
 namespace Nethermind.Init.Steps
 {
-    [RunnerStepDependencies(typeof(InitializeBlockProducer))]  // Unfortunately EngineRPC API need review blockTree
+    [RunnerStepDependencies(typeof(InitializeBlockProducer), typeof(ReviewBlockTree))]  // Unfortunately EngineRPC API need review blockTree
     public class StartBlockProducer : IStep
     {
         protected IApiWithBlockchain _api;
