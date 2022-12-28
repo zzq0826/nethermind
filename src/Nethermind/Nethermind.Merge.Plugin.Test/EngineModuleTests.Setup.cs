d9979c6d4afd86a1b8f966308b1e8815edff17ed
Tanishq Jasoria <jasoriatanishq@gmail.com>
2022-12-28 13:40:18 +0530

    allow access to state and storage through WorldState
    use IWorldState instead

--- src/Nethermind/Nethermind.Merge.Plugin.Test/EngineModuleTests.Setup.cs
+++ src/Nethermind/Nethermind.Merge.Plugin.Test/EngineModuleTests.Setup.cs
@@ -199,9 +199,8 @@ public partial class EngineModuleTests
                 SpecProvider,
                 BlockValidator,
                 NoBlockRewards.Instance,
-                new BlockProcessor.BlockValidationTransactionsExecutor(TxProcessor, State),
-                State,
-                Storage,
+                new BlockProcessor.BlockValidationTransactionsExecutor(TxProcessor, new WorldState(State, Storage)),
+                WorldState,
                 ReceiptStorage,
                 NullWitnessCollector.Instance,
                 LogManager);
