8f03c5c2d28e3ebb3e95178bb663a46f23928582
Jorge Mederos <jmederosalvarado@gmail.com>
2023-04-06 10:30:05 -0400

    Add mumbai config

--- src/Nethermind/Nethermind.Consensus.Bor/BorConfig.cs
+++ src/Nethermind/Nethermind.Consensus.Bor/BorConfig.cs
@@ -2,5 +2,5 @@ namespace Nethermind.Consensus.Bor;
 
 public class BorConfig : IBorConfig
 {
-    public string HeimdallUrl { get; set; } = "http://localhost:8545";
+    public string HeimdallUrl { get; set; } = "http://127.0.0.1:1317";
 }
--- src/Nethermind/Nethermind.Consensus.Bor/IBorConfig.cs
+++ src/Nethermind/Nethermind.Consensus.Bor/IBorConfig.cs
@@ -4,6 +4,6 @@ namespace Nethermind.Consensus.Bor;
 
 public interface IBorConfig : IConfig
 {
-    [ConfigItem(Description = "The URL of the Heimdall node to connect to.", DefaultValue = "http://localhost:8545")]
+    [ConfigItem(Description = "The URL of the Heimdall node to connect to.", DefaultValue = "http://127.0.0.1:1317")]
     string HeimdallUrl { get; set; }
 }
