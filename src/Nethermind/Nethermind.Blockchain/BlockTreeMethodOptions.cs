06ec3d92d2df836d42ce228e775983d29a6ca40d
Tanishq Jasoria <jasoriatanishq@gmail.com>
2023-07-06 14:25:32 +0530

    add staless flag

--- src/Nethermind/Nethermind.Blockchain/BlockTreeMethodOptions.cs
+++ src/Nethermind/Nethermind.Blockchain/BlockTreeMethodOptions.cs
@@ -69,6 +69,11 @@ public enum BlockTreeSuggestOptions
     /// Force not to set as main block
     /// </summary>
     ForceDontSetAsMain = 8,
+
+    /// <summary>
+    /// If block should be processed in a stateless manner
+    /// </summary>
+    ShouldProcessStateless = 16,
 }
 
 public static class BlockTreeSuggestOptionsExtensions
