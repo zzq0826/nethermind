5d6910685d137f3b5c223fefb02382a3cc6378b4
Amirul Ashraf <asdacap@gmail.com>
2023-09-28 12:59:20 +0800

    Compact list

---
+++ src/Nethermind/Nethermind.Core.Test/Collections/CompactListTests.cs
@@ -0,0 +1,141 @@
+// SPDX-FileCopyrightText: 2023 Demerzel Solutions Limited
+// SPDX-License-Identifier: LGPL-3.0-only
+
+using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using Nethermind.Core.Collections;
+using NUnit.Framework;
+
+namespace Nethermind.Core.Test.Collections;
+
+public class CompactListTests
+{
+    private CompactList<int> OneThousandItems()
+    {
+        CompactList<int> list = new CompactList<int>(7);
+
+        for (int i = 0; i < 1000; i++)
+        {
+            list.Add(i);
+        }
+
+        return list;
+    }
+
+    [Test]
+    public void TestAddSequence()
+    {
+        IList<int> list = OneThousandItems();
+
+        list.Count.Should().Be(1000);
+        for (int i = 0; i < 1000; i++)
+        {
+            list[i].Should().Be(i);
+        }
+
+        int cidx = 0;
+        foreach (int idx in list)
+        {
+            idx.Should().Be(cidx);
+            cidx++;
+        }
+    }
+
+    [Test]
+    public void TestIterate()
+    {
+        IList<int> list = OneThousandItems();
+        int cidx = 0;
+        foreach (int idx in list)
+        {
+            idx.Should().Be(cidx);
+            cidx++;
+        }
+
+        cidx.Should().Be(1000);
+    }
+
+    [Test]
+    public void TestRemoveFromMiddle()
+    {
+        IList<int> list = OneThousandItems();
+        for (int i = 0; i < 500; i++)
+        {
+            list.Remove(i);
+        }
+
+        for (int i = 0; i < 500; i++)
+        {
+            list[i].Should().Be(i+500);
+        }
+    }
+
+    [Test]
+    public void TestClear()
+    {
+        IList<int> list = OneThousandItems();
+        list.Count.Should().Be(1000);
+        list.Clear();
+        list.Count.Should().Be(0);
+    }
+
+    [Test]
+    public void TestBoundCheck()
+    {
+        IList<int> list = OneThousandItems();
+
+        Func<int> act = () => list[-1];
+        act.Should().Throw<IndexOutOfRangeException>();
+
+        act = () => list[1000];
+        act.Should().Throw<IndexOutOfRangeException>();
+
+        act = () => list[1001];
+        act.Should().Throw<IndexOutOfRangeException>();
+
+        act = () => list[0];
+        act.Should().NotThrow();
+    }
+
+    [Test]
+    public void TestAddFromMiddle()
+    {
+        IList<int> list = OneThousandItems();
+
+        list.Count.Should().Be(1000);
+        for (int i = 0; i < 500; i++)
+        {
+            list.Insert(500, i);
+        }
+
+        for (int i = 0; i < 500; i++)
+        {
+            list[i].Should().Be(i);
+            list[i+500].Should().Be(500-i-1);
+            list[i+1000].Should().Be(i+500);
+        }
+    }
+
+    [Test]
+    public void RandomSort()
+    {
+        CompactList<int> list = new CompactList<int>(7);
+        List<int> list2 = new List<int>();
+
+        for (int i = 0; i < 1000; i++)
+        {
+            int num = Random.Shared.Next(1000);
+            list.Add(num);
+            list2.Add(num);
+        }
+
+        list.Sort();
+        list2.Sort();
+
+        for (int i = 0; i < 1000; i++)
+        {
+            list[i].Should().Be(list2[i]);
+        }
+    }
+}
---
+++ src/Nethermind/Nethermind.Core/Collections/CompactList.cs
@@ -0,0 +1,193 @@
+// SPDX-FileCopyrightText: 2023 Demerzel Solutions Limited
+// SPDX-License-Identifier: LGPL-3.0-only
+
+using System;
+using System.Buffers;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Nethermind.Core.Collections;
+
+/// <summary>
+/// An IList implementation that internally is an array of array.
+/// "but why?" you might ask... well my friend, its to prevent Large Object Heap allocation, **nasty little bugger**.
+/// This class lets you specify the size of the inner array so that its not big enough to become a LOH.
+/// Also, pool the inner array by default.
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public class CompactList<T>: IList<T>
+{
+    private ArrayPool<T> _pool;
+    private readonly int _itemPerInnerNode;
+    private T[]?[] _arrayOfArray;
+    private int _count;
+
+    public CompactList(int itemPerInnerNode = 8 * 1028)
+    {
+        _itemPerInnerNode = itemPerInnerNode;
+        _arrayOfArray = Array.Empty<T[]?>();
+        _pool = ArrayPool<T>.Shared;
+        _count = 0;
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        for (int i = 0; i < _count; i++)
+        {
+            yield return this[i];
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        for (int i = 0; i < _count; i++)
+        {
+            yield return this[i];
+        }
+    }
+
+    private void ExpandForLength(int requiredLength)
+    {
+        int neededInnerLength = (_itemPerInnerNode + requiredLength) / _itemPerInnerNode;
+
+        if (neededInnerLength <= _arrayOfArray.Length) return;
+
+        T[]?[] newArrayOfArray = new T[Math.Max(_arrayOfArray.Length * 2, 1)][];
+        _arrayOfArray.CopyTo(newArrayOfArray.AsSpan());
+        _arrayOfArray = newArrayOfArray;
+    }
+
+    public void Add(T item)
+    {
+        ExpandForLength(_count+1);
+
+        int slotIdx = _count / _itemPerInnerNode;
+        _arrayOfArray[slotIdx] ??= _pool.Rent(_itemPerInnerNode);
+        _arrayOfArray[slotIdx]![_count%_itemPerInnerNode] = item;
+        _count++;
+    }
+
+    public void Clear()
+    {
+        foreach (T[]? innerArray in _arrayOfArray)
+        {
+            if (innerArray == null) break;
+            _pool.Return(innerArray);
+        }
+        _arrayOfArray = Array.Empty<T[]>();
+        _count = 0;
+    }
+
+    public bool Contains(T item)
+    {
+        return IndexOf(item) != -1;
+    }
+
+    public void CopyTo(T[] array, int arrayIndex)
+    {
+        if (array.Length - arrayIndex < _count) throw new ArgumentException("insufficient space in target");
+
+        for (int i = 0; i < _arrayOfArray.Length; i++)
+        {
+            if (_arrayOfArray[i] == null) break;
+
+            if (_count / _itemPerInnerNode == i)
+            {
+                // last one
+                _arrayOfArray[i]!.AsSpan(0, _count % _itemPerInnerNode).CopyTo(array.AsSpan());
+            }
+            else
+            {
+                _arrayOfArray[i]!.CopyTo(array, arrayIndex + i * _itemPerInnerNode);
+            }
+        }
+
+    }
+
+    public bool Remove(T item)
+    {
+        int idx = IndexOf(item);
+        if (idx == -1) return false;
+
+        RemoveAt(idx);
+        return true;
+    }
+
+    public int Count => _count;
+    public bool IsReadOnly => false;
+    public int IndexOf(T item)
+    {
+        for (int i = 0; i < _arrayOfArray.Length; i++)
+        {
+            if (_arrayOfArray[i] == null) break;
+            int innerIndex = Array.IndexOf(_arrayOfArray[i]!, item);
+            if (innerIndex != -1) return i * _itemPerInnerNode + innerIndex;
+        }
+
+        return -1;
+    }
+
+    public void Insert(int index, T item)
+    {
+        if (index < 0 || index > _count) throw new IndexOutOfRangeException();
+        ShiftRight(index);
+        this[index] = item;
+    }
+
+    public void RemoveAt(int index)
+    {
+        if (index < 0 || index > _count) throw new IndexOutOfRangeException();
+        ShiftLeft(index);
+    }
+
+    private void ShiftRight(int startingPosition)
+    {
+        _count++;
+        ExpandForLength(_count);
+        _arrayOfArray[_count / _itemPerInnerNode] ??= _pool.Rent(_itemPerInnerNode);
+
+        for (int i = _count-1; i > startingPosition; i--)
+        {
+            this[i] = this[i-1];
+        }
+    }
+
+    private void ShiftLeft(int startingPosition)
+    {
+        for (int i = startingPosition; i < _count-1; i++)
+        {
+            this[i] = this[i + 1];
+        }
+
+        // Maybe shrink
+        if (_count % _itemPerInnerNode == 0)
+        {
+            int prevLastArray = _count / _itemPerInnerNode;
+            T[] lastArr = _arrayOfArray[prevLastArray]!;
+            _pool.Return(lastArr);
+            _arrayOfArray[prevLastArray] = null;
+        }
+        _count--;
+
+        // Not shrinking _arrayOfArray. Its probably fine.
+    }
+
+    public T this[int index]
+    {
+        get
+        {
+            if (index < 0 || index >= _count) throw new IndexOutOfRangeException();
+            return _arrayOfArray[index / _itemPerInnerNode]![index % _itemPerInnerNode];
+        }
+        set
+        {
+            if (index < 0 || index >= _count) throw new IndexOutOfRangeException();
+            _arrayOfArray[index / _itemPerInnerNode]![index % _itemPerInnerNode] = value;
+        }
+    }
+
+    public void Sort()
+    {
+        ListSortHelper<T>.Default.Sort(this, 0, _count, null);
+    }
+}
