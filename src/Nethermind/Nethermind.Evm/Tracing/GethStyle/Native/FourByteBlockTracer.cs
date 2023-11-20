3e1680009abeae9365a06250c74b1b936932043e
Marc Harvey-Hill <marc@Marcs-MacBook-Pro.local>
2023-11-20 11:56:32 +0000

    finished 4byteTracer

--- src/Nethermind/Nethermind.Evm/Tracing/GethStyle/Native/FourByteBlockTracer.cs
+++ src/Nethermind/Nethermind.Evm/Tracing/GethStyle/Native/FourByteBlockTracer.cs
@@ -16,12 +16,25 @@ public class FourByteBlockTracer : GethLikeBlockTracerBase<FourByteTxTracer>
 
 public sealed class FourByteTxTracer : GethLikeTxTracer
 {
-    private Dictionary<byte[], int> ids = new();
+    private Dictionary<string, int> ids = new();
     private ReadOnlyMemory<byte> input;
 
     public FourByteTxTracer(GethTraceOptions options) : base(options)
     {
         IsTracingActions = true;
+    }
+
+    private void RegisterFourByteId(ReadOnlyMemory<byte> data, int length)
+    {
+        string key = Convert.ToHexString(data.ToArray()).ToLower() + "-" + length.ToString();
+        if (ids.ContainsKey(key))
+        {
+            ids[key]++;
+        }
+        else
+        {
+            ids.Add(key, 1);
+        }
     }
 
     public override void ReportAction(long gas, UInt256 value, Address from, Address to, ReadOnlyMemory<byte> input, ExecutionType callType, bool isPrecompileCall = false)
@@ -51,7 +64,7 @@ public sealed class FourByteTxTracer : GethLikeTxTracer
                 return;
             }
 
-            ids.Add(input.Slice(0, 4).ToArray(), input.Length - 4);
+            RegisterFourByteId(input.Slice(0, 4), input.Length - 4);
         }
     }
 
@@ -59,11 +72,11 @@ public sealed class FourByteTxTracer : GethLikeTxTracer
     {
         if (input.Length >= 4)
         {
-            ids.Add(input.Slice(0, 4).ToArray(), input.Length - 4);
+            RegisterFourByteId(input.Slice(0, 4), input.Length - 4);
         }
 
         GethLikeTxTrace result = base.BuildResult();
-        result.CustomTracerResult = new GethLikeJavascriptTrace { Value = new { ids } };
+        result.CustomTracerResult = new GethLikeJavascriptTrace { Value = ids };
         return result;
     }
 }
