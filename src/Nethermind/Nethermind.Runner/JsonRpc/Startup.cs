3e58cce49137d6fd89e61ee9cb697e726da0c1b5
Ben Adams <thundercat@illyriad.co.uk>
2024-01-25 15:17:52 +0000

    Revert "Update Startup.cs"
    This reverts commit ac6c6e165faef9a7cffda330093c3284513658b6.

--- src/Nethermind/Nethermind.Runner/JsonRpc/Startup.cs
+++ src/Nethermind/Nethermind.Runner/JsonRpc/Startup.cs
@@ -64,7 +64,7 @@ namespace Nethermind.Runner.JsonRpc
                 logging.FileSizeLimit = 5 * 1024 * 1024;
                 logging.RetainedFileCountLimit = 2;
                 logging.FileName = "MyLogFile";
-                logging.LogDirectory = @".";
+                logging.LogDirectory = @"C:\logs";
                 logging.FlushInterval = TimeSpan.FromSeconds(2);
             });
 
