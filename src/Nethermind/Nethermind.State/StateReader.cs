8b5fb0e743352ea29a1f511f9af623da3fa775c2
Jorge Mederos <jmederosalvarado@gmail.com>
2024-02-22 15:20:58 +0100

    log stack traces

--- src/Nethermind/Nethermind.State/StateReader.cs
+++ src/Nethermind/Nethermind.State/StateReader.cs
@@ -34,7 +34,7 @@ namespace Nethermind.State
         public AccountStruct? GetAccount(Hash256 stateRoot, Address address)
         {
             AccountStruct? account = GetState(stateRoot, address);
-            _logger.Info($"GetAccount: {address} {account}");
+            _logger.Info($"GetAccount: {address} {account}\n{new System.Diagnostics.StackTrace()}");
             return account;
         }
 
